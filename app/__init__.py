0edde81cc9774ae5ee322f2618000b3399ae33dd
mastercode5 <43855236+mastercode5@users.noreply.github.com>
2022-10-08 19:26:34 +0200

    create db

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,8 @@ MYDIR = os.path.dirname(__file__)
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = os.path.dirname(__file__) +"/database.sql"
+app.config["SECRET_KEY"] = 'secret'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + MYDIR + '/database.sql'
 app.config['UPLOAD_FOLDER'] = os.path.join(MYDIR+'/'+ "static/images")
 app.config['ALLOWED_EXTENSIONS'] = set(['txt', 'pdf', 'png', 'jpg', 'jpeg'])
 app.config['STATIC_URL_PATH'] = os.path.join(MYDIR+'/'+ "static")
@@ -17,6 +18,12 @@ app.config['STATIC_URL_PATH'] = os.path.join(MYDIR+'/'+ "static")
 db       = SQLAlchemy(app)
 migation = Migrate(app, db)
 
+
+
+
 from app import models, routes
+
+with app.app_context():
+    db.create_all()
 
 app.run()
--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
 
-class User(db.model):
+class User(db.Model):
     id          = db.Column(db.Integer, primary_key = True)
     name        = db.Column(db.String(256), unique=True)
     mail        = db.Column(db.String(256), unique=True)
@@ -10,7 +10,7 @@ class User(db.model):
     description = db.Column(db.String(4096), unique = False)
 
 
-class event(db.model):
+class event(db.Model):
     id          = db.Column(db.Integer, primary_key = True)
     title       = db.Column(db.String(256), unique = True)
     creator     = db.Column(db.String(256), unique = False)
@@ -18,4 +18,4 @@ class event(db.model):
     description = db.Column(db.Text, unique = True)
     date        = db.Column(db.DateTime, unique = False)
     location    = db.Column(db.String(1024), unique = False)
-    
+
