4a46786ca5675cfe9c165512f833882d7cf13211
mastercode5 <43855236+mastercode5@users.noreply.github.com>
2022-10-09 21:16:35 +0200

    add set and check hash password

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
 
+#User definition
 class User(db.Model):
     id          = db.Column(db.Integer, primary_key = True)
     name        = db.Column(db.String(256), unique=True)
@@ -9,6 +10,14 @@ class User(db.Model):
     city        = db.Column(db.String(1024), unique=False)
     description = db.Column(db.String(4096), unique = False)
 
+    def set_password(self):
+        self.password = generate_password_hash(self.password, 'sha256')
+
+    def check_password(self, given_password):
+        given_password_hash = generate_password_hash(given_password, 'sha256')
+        if check_password_hash(self.password, given_password):
+            return True
+        return False
 
 class event(db.Model):
     id          = db.Column(db.Integer, primary_key = True)
