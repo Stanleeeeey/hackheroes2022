1eb3a93db268bae9c03fc84edf2beac941e65c23
mastercode5 <43855236+mastercode5@users.noreply.github.com>
2022-10-12 20:00:03 +0200

    added menu on screens <1150px

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,1 +1,18 @@
-//jakbys potrzebowal js do index.html
+//js for index.html
+//main coder Stanislaw Kawulok
+
+function ShowMenu(){
+    x = document.getElementById('hidden-menu');
+
+    if(x.classList.contains('hidden-menu')){
+
+        x.classList.remove("hidden-menu");
+        x.classList.add('visible-menu');
+    }else{
+
+
+        x.classList.remove("visible-menu");
+        x.classList.add('hidden-menu');
+    }
+    
+}
