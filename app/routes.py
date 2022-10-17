ab8634bfe808fa205e35a34de03e7e4931785f9f
mastercode5 <43855236+mastercode5@users.noreply.github.com>
2022-10-17 19:45:50 +0200

    resolwing conflicts

--- app/routes.py
+++ app/routes.py
@@ -3,45 +3,55 @@
 
 from app import app, load_user
 from flask import render_template, request, flash
-from flask_login import login_user
-from app.models import Login, GetUserByUserName, GetAllEvents
+from flask_login import login_user, current_user
+from app.models import Login, GetUserByUserName, AddUser
 import jinja2
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-
-
 class LoginForm(FlaskForm):
     username = StringField('Username')
     password = PasswordField('Password')
     submit = SubmitField('Submit')
 
 #main webpage
+def IsLoged():
+  return current_user.is_authenticated 
 
 
 @app.route('/')
 def home_page():
-    return render_template('index.html')
+    return render_template('index.html', loged = IsLoged())
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if request.method == 'GET':
-        return render_template('login.html', form = form) 
+        return render_template('login.html', form = form, loged = IsLoged()) 
     else:
         user = request.form.get('username')
         password = request.form.get('password')
         if Login(user, password):
             login_user(GetUserByUserName(user))
-            return render_template('index.html')
+            return render_template('index.html', loged = IsLoged())
         else:
             flash('Zły login lub hasło')
-            return render_template('login.html', form = form)         
-@app.route('/signup')
+            return render_template('login.html', form = form, loged = IsLoged())   
+
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = LoginForm()
-    return render_template('login.html', form = form)
+    if request.method == 'GET':
+        return render_template('signup.html', form = form, loged = IsLoged())
+    else:
+        user = request.form.get('username')
+        password = request.form.get('password')
+        if AddUser(user, '', password, '', '', 'user'):
+            return render_template('login.html', form = form, loged = IsLoged())
+        else:
+            flash('Zły login lub hasło')
+            return render_template('signup.html', form = form, loged = IsLoged())
 
 @app.route('/events')
 def all_events():
-    return render_template('events.html', events= GetAllEvents())
+    return render_template('events.html', events= GetAllEvents(), loged = IsLoged())
 
