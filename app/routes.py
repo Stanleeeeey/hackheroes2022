7934fa593524b8315cc289375f3b42c29fa9fec7
mastercode5 <43855236+mastercode5@users.noreply.github.com>
2022-10-18 16:44:56 +0200

    added diffrent actions for loged users

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,10 @@
 # file responsible for routing and operations related to User actions
 # coders responsible for file : Michal Koren, Stanislaw Kawulok
 
+import re
 from app import app, load_user
-from flask import render_template, request, flash
-from flask_login import login_user, current_user
+from flask import render_template, request, flash, redirect
+from flask_login import login_user, current_user, logout_user, login_required
 from app.models import Login, GetUserByUserName, AddUser, GetAllEvents, GetEventByTitle
 import jinja2
 from flask_wtf import FlaskForm
@@ -17,8 +18,7 @@ class LoginForm(FlaskForm):
 def IsLoged():
   return current_user.is_authenticated 
 
-def IsLoged():
-  return current_user.is_authenticated 
+
 
 @app.route('/')
 def home_page():
@@ -34,7 +34,7 @@ def login():
         password = request.form.get('password')
         if Login(user, password):
             login_user(GetUserByUserName(user))
-            return render_template('index.html', loged = IsLoged())
+            return redirect('/')
         else:
             flash('Zły login lub hasło')
             return render_template('login.html', form = form, loged = IsLoged())   
@@ -48,10 +48,19 @@ def signup():
         user = request.form.get('username')
         password = request.form.get('password')
         if AddUser(user, '', password, '', '', 'user'):
-            return render_template('login.html', form = form, loged = IsLoged())
+            return redirect('/login')
         else:
             flash('Zły login lub hasło')
             return render_template('signup.html', form = form, loged = IsLoged())
+
+
+@app.route('/logout')
+#@login_required
+def logout():
+    print('login out')
+    logout_user()
+    print(IsLoged())
+    return redirect('/')
 
 @app.route('/events')
 def all_events():
